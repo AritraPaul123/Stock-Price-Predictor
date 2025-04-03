07dc4e827884de3b954a084fa280d927dd178a15
Aritra Paul <aritrap581@gmail.com>
2025-04-03 19:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,8 @@
-
+import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-import yfinance as yf
-from datetime import datetime
-import ta
-import streamlit as st
+from model import get_stock_data, train_model  # Import from your stock_model.py file
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-
-# Function to fetch stock data
-def get_stock_data(ticker="AAPL", start="2020-01-01"):
-    end = datetime.today().strftime('%Y-%m-%d')
-    stock = yf.download(ticker, start=start, end=end)
-    return stock
-
-def train_model(df):
-    df['Target'] = df['Close'].shift(-1)  # Predict next day's Close price
-    df.dropna(inplace=True)  # Remove NaN values
-    print(df.isna().sum())  # Check if there are NaN values in the DataFrame
-    print(df.tail())
-
-    X = df[['Open', 'High', 'Low', 'Close', 'Volume']]  # Features
-    y = df['Target']  # Target variable
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
-
-    model = LinearRegression()
-    model.fit(X_train, y_train)
-    return model, X_test, y_test
 
 # List of tickers with company names
 tickers = [
