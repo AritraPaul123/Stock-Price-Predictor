8d35a913779c5b5d877e09a7844db1222de68172
Aritra Paul <aritrap581@gmail.com>
2025-04-04 13:53:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ df = get_stock_data(ticker[0])  # Use the ticker symbol (first element of the tu
 if df.empty:
     st.error(f"Could not fetch data for {ticker[0]}. Please try again.")
 else:
-    st.write(f"Data for {ticker[0]} from {df.index.max()-4} to {df.index.max()}")
+    st.write(f"Data for {ticker[0]} from {df.tail(5).index.min()} to {df.tail(5).index.max()}")
     st.dataframe(df.tail())
 
     # Train the model
